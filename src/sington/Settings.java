f1c88b4caa6ec2d203c0f809b7ac5f88d69eb0c8
hohshho <tkdgur8377@gmail.com>
2025-03-24 23:47:26 +0900

    멀티 쓰레드 환경에서 안전하게 구현하는 방법

---
+++ src/sington/Settings.java
@@ -0,0 +1,13 @@
+package sington;
+
+public class Settings {
+    private Settings() {}
+
+    private static class SettingsHolder {
+        private static final Settings INSTANCE = new Settings();
+    }
+
+    public static Settings getInstance() {
+        return SettingsHolder.INSTANCE;
+    }
+}
